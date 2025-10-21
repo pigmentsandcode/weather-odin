6606229958951a0627494940d9343bb62e2d0498
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-10-21 14:47:31 -0500

    Add func to display weather data

--- src/weatherDisplay.js
+++ src/weatherDisplay.js
@@ -2,12 +2,12 @@ export function getWeatherHTML(weatherData) {
   return `
     <h2>${weatherData.resolvedAddress}</h2>
         <div class="weather-details">
-          <p id="current-temp">${weatherData.temp}</p>
+          <p id="current-temp">${weatherData.temp}&deg; F</p>
           <p id="weather-icon">${weatherData.icon}</p>
           <p id="current-conditions">${weatherData.conditions}</p>
-          <p id="feels-like">${weatherData.feelsLike}</p>
-          <p id="low-high">${weatherData.low}/${weatherData.high}</p>
-          <p id="precip-chance">${weatherData.precipChance}</p>
+          <p id="feels-like">${weatherData.feelsLike}&deg; F</p>
+          <p id="low-high">${weatherData.low}&deg; F / ${weatherData.high}&deg; F</p>
+          <p id="precip-chance">${weatherData.precipChance}%</p>
           <p id="forecast">${weatherData.forecastDescript}</p>
         </div>`;
 }
