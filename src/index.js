4eb7154a2644caa15d1645a37083c9a9277b7964
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-10-21 14:53:33 -0500

    Add search and display functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import "./styles.css";
+import { getWeatherHTML } from "./weatherDisplay";
 
 async function getWeatherData(location) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current%2Cdays&key=YOURKEYHERE&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current%2Cdays&key=YOUR_KEY_HERE&contentType=json`
     );
     const jsonRes = await response.json();
-    return processData(jsonRes);
+    const weatherSectionEl = document.querySelector("#weather-info");
+    const weatherHTML = getWeatherHTML(processData(jsonRes));
+    weatherSectionEl.insertAdjacentHTML("beforeend", weatherHTML);
   } catch (err) {
     console.log(err);
   }
@@ -14,6 +17,7 @@ async function getWeatherData(location) {
 
 function processData(responseData) {
   const weather = {
+    resolvedAddress: responseData.resolvedAddress,
     temp: responseData.currentConditions.temp,
     feelsLike: responseData.currentConditions.feelslike,
     conditions: responseData.currentConditions.conditions,
@@ -25,3 +29,20 @@ function processData(responseData) {
   };
   return weather;
 }
+
+function handleClickSearch(e) {
+  e.preventDefault();
+  const cityInput = document.querySelector("#city");
+  if (cityInput.validity.patternMismatch) {
+    cityInput.setCustomValidity("Please enter a city name!");
+  } else {
+    const city = cityInput.value;
+    const state = document.querySelector("#state").value;
+    const locationToSearch = city.trim() + ", " + state;
+    console.log("location: " + locationToSearch);
+    getWeatherData(locationToSearch);
+  }
+}
+
+const searchBtnEl = document.querySelector("#search-btn");
+searchBtnEl.addEventListener("click", handleClickSearch);
