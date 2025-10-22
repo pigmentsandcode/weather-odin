b1d48384b8e90964ed242f55fa8ab6441309b916
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-10-22 13:27:25 -0500

    Add display with icon

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import "./styles.css";
-import { getWeatherHTML } from "./weatherDisplay";
+import { getWeatherHTML, getDummyHTML } from "./weatherDisplay";
 
 async function getWeatherData(location) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current%2Cdays&key=YOUR_KEY_HERE&contentType=json`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current%2Cdays&iconSet=icons2&key=YOUR_KEY_HERE&contentType=json`
     );
     const jsonRes = await response.json();
     const weatherSectionEl = document.querySelector("#weather-info");
-    const weatherHTML = getWeatherHTML(processData(jsonRes));
+    const processedData = await processData(jsonRes);
+    const weatherHTML = getWeatherHTML(processedData);
+    weatherSectionEl.innerHTML = "";
     weatherSectionEl.insertAdjacentHTML("beforeend", weatherHTML);
   } catch (err) {
     console.log(err);
   }
 }
 
-function processData(responseData) {
+async function processData(responseData) {
+  let icon = await import(`./icons/${responseData.currentConditions.icon}.png`);
+  const iconSrc = icon.default;
   const weather = {
     resolvedAddress: responseData.resolvedAddress,
     temp: responseData.currentConditions.temp,
     feelsLike: responseData.currentConditions.feelslike,
     conditions: responseData.currentConditions.conditions,
-    icon: responseData.currentConditions.icon,
+    icon: iconSrc,
     high: responseData.days[0].tempmax,
     low: responseData.days[0].tempmin,
     precipChance: responseData.currentConditions.precipprob,
--- src/weatherDisplay.js
+++ src/weatherDisplay.js
@@ -3,11 +3,11 @@ export function getWeatherHTML(weatherData) {
     <h2>${weatherData.resolvedAddress}</h2>
         <div class="weather-details">
           <p id="current-temp">${weatherData.temp}&deg; F</p>
-          <p id="weather-icon">${weatherData.icon}</p>
+          <p id="weather-icon"><img src=${weatherData.icon} /></p>
           <p id="current-conditions">${weatherData.conditions}</p>
-          <p id="feels-like">${weatherData.feelsLike}&deg; F</p>
+          <p id="feels-like">Feels like ${weatherData.feelsLike}&deg; F</p>
           <p id="low-high">${weatherData.low}&deg; F / ${weatherData.high}&deg; F</p>
-          <p id="precip-chance">${weatherData.precipChance}%</p>
+          <p id="precip-chance">${weatherData.precipChance}% Precipitation</p>
           <p id="forecast">${weatherData.forecastDescript}</p>
         </div>`;
 }
